0e803c57a4d48c0937bd3c08d8482d082c1a2d22
ZhangXuecheng <zxc0gary@connect.hku.hk>
2026-02-03 22:30:26 +0800

    rewrite parsing

--- counting-days/main.py
+++ counting-days/main.py
@@ -1,42 +1,51 @@
 from datetime import datetime, timedelta
+from typing import Any
+
 from .event import Event
 
-data: list[dict[str, str]] = [
+data: list[dict[str, str | bool]] = [
     {"fulldate": "2026-01-01", "event": "New Year's Day"},
     {"fulldate": "2026-02-03", "event": "Special"},
     {"fulldate": "2026-07-04", "event": "Independence Day"},
     {"fulldate": "2026-12-25", "event": "Christmas Day"},
+    {"fulldate": "1998-09-30", "event": "My Birthday", "yearly": True},
 ]
 
 
-def parse_data(events: list[dict[str, str]]) -> list[Event]:
-
-    def days_wrt_today(target_date_str: str) -> timedelta:
-        today = datetime.today().date()
-        target_date = datetime.strptime(target_date_str, "%Y-%m-%d").date()
-        return target_date - today
-
-    return [
-        Event(
-            fulldate=event["fulldate"],
-            date=datetime.strptime(event["fulldate"], "%Y-%m-%d").date(),
-            title=event["event"],
-            tdelta=days_wrt_today(event["fulldate"])
+def parse_data(events: list[dict[str, Any]]) -> list[Event]:
+    today = datetime.now().date()
+    enriched_events: list[Event] = []
+    for e in events:
+        date = datetime.fromisoformat(e["fulldate"]).date()
+        title = e["event"]
+        yearly = e.get("yearly", False)
+        if yearly:
+            date = date.replace(year=today.year)
+            if date < today:
+                date = date.replace(year=today.year + 1)
+        tdelta = date - today
+        enriched_events.append(
+            Event(date=date, title=title, tdelta=tdelta, yearly=yearly)
         )
-        for event in events
-    ]
+    return enriched_events
+
 
 def list_events(events: list[Event]) -> None:
-    events.sort(key=lambda e: (e.tdelta 
-                                   if e.tdelta.days != 0
-                                   else timedelta.min, 
-                               e.title))
+    events.sort(
+        key=lambda e: (
+            e.tdelta if e.tdelta.days != 0 else timedelta.min,
+            e.title,
+        )
+    )
     for event in events:
         if event.tdelta.days == 0:
             print(f"    Today is {event.title}!")
         else:
-            prep = "since" if event.tdelta.days < 0 else "before"
-            print(f"{abs(event.tdelta.days):>4} days {prep:<7} {event.title}")
+            if event.tdelta.days < 0:
+                print(f"Days after  {event.title:<20}: {-event.tdelta.days:>4}")
+            else:
+                print(f"Days before {event.title:<20}: {event.tdelta.days:>4}")
+
 
 def main() -> None:
     enriched_events = parse_data(data)
