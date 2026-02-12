7fc30684790e68d3b4479aa2d9f8db02cba25412
ZhangXuecheng <zxc0gary@connect.hku.hk>
2026-02-12 22:11:39 +0800

    update: get event list from command argument

--- counting-days/main.py
+++ counting-days/main.py
@@ -1,11 +1,15 @@
+import sys
 from datetime import timedelta
 
 from .display_events import display_events
 from .get_events import read_events_from_file
 
-
-def main() -> None:
-    events = read_events_from_file("events/test.events")
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        events = read_events_from_file(sys.argv[1])
+    else:
+        print("Usage: python main.py <events_file>")
+        sys.exit(1)
     events.sort(
         key=lambda e: (
             e.tdelta if e.tdelta.days != 0 else timedelta.min,
@@ -13,7 +17,3 @@ def main() -> None:
         )
     )
     display_events(events)
-
-
-if __name__ == "__main__":
-    main()
